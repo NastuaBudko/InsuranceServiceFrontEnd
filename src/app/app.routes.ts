e098bd61b5a45fac19ecd609f81dcfb9b082621d
Anastasiya Budko <nastuabudko@gmail.com>
2024-07-22 19:51:41 +0300

    Created personal account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { InsurancesListComponent } from './components/insurances-list/insurances-list.component';
 import { InsuranceDetailsComponent } from './components/insurances-list/insurance/insurance-details/insurance-details.component';
+import { PersonalAccountComponent } from './components/personal-account/personal-account.component';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,10 @@ export const routes: Routes = [
     {
         path: 'insurances/:insuranceId',
         component: InsuranceDetailsComponent
+    },
+    {
+        path: 'profile',
+        component: PersonalAccountComponent
     },
     
 ];
